42be7d202f3f0ceb5d49c9f2b741e712e5e29fd6
Jo Ilhyun <onemonsterceo@gmail.com>
2017-12-21 22:02:38 +0900

    Update Food.js

--- src/components/Food.js
+++ src/components/Food.js
@@ -3,46 +3,34 @@ import React,{Component} from 'react';
 import Rating from '../dialog/Rating';
 
 class Menu extends Component {
-
-    constructor(prpos) {
-        super(prpos)
-        this.state = {
-            isOpen: false
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false
     }
-
-
-
-    render() {
-
-
-
-        return (
-            <div onClick={rating.bind(this)}>
-                <li><a>{this.props.food.price + " "}</a>
-                    <a>{this.props.food.name + " "}</a>
-                    <a>{this.props.food.rating}</a>
-                    <Rating show={this.state.isOpen}
-                           onClose={rating.bind(this)}
-                    name = {this.props.food.name}
-                    rate = {this.props.food.rating}>
-                    </Rating>
-                </li>
-            </div>)
-    }
-
-
+    this.rateMeal = this.rate.bind(this)
+  }
+  rateMeal () {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+  render() {
+    const { food } = this.props
+    return (
+      <div onClick={rating.bind(this)}>
+        <li>
+          <a>{food.price + " "}</a>
+          <a>{food.name + " "}</a>
+          <a>{food.rating}</a>
+          <Rating
+            show={this.state.isOpen}
+            onClose={() => rateMeal()}
+            name={food.name}
+            rate={food.rating}
+          />
+        </li>
+      </div>
+    )
+  }
 }
-
-
-function rating(){
-    this.setState({
-        ...this.state,
-        isOpen: !this.state.isOpen
-
-    })
-}
-
-
 
 export default Menu
